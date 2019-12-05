e748a21fefcfdb341a9f915bca16cc789798f862
gordon <gordonchen@trussan.com>
2019-12-05 17:03:02 +0800

    't'

--- resource/view/index.blade.php
+++ resource/view/index.blade.php
@@ -1,29 +1,34 @@
-<!--jquery需要引入的文件-->
-<script src="/js/jquery-3.2.1.js"></script>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <script src="/js/jquery-3.2.1.js"></script>
+    </head>
+    <body>
+        <a href="119.23.225.253:83">web高级vue</a>
+        <script type="text/javascript" src="/js/jquery.form.js"></script>
+        <div style="display:flex;justify-content: center; align-items:center; width: 100%;height: 100%;border: 1px solid white;text-align:center;">
+            <div>
+                <span style="line-height: 50px">ExcelToMysql</span>
+                <form action="" method="post" enctype="multipart/form-data" id="ajaxForm">
+                    <input type="text" name="frameId" id="frameId">
+                    <input type="file" name="excel" required>
+                    <input type="submit" id="ajaxSubmit">
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
 
-<!--ajax提交表单需要引入jquery.form.js-->
-<a href=":83">web高级</a>
-<script type="text/javascript" src="/js/jquery.form.js"></script>
-<div style="display:flex;justify-content: center; align-items:center; width: 100%;height: 100%;border: 1px solid white;text-align:center;">
-    <div>
-        <span style="line-height: 50px">ExcelToMysql</span>
-        <form action="" method="post" enctype="multipart/form-data" id="ajaxForm">
-            <input type="text" name="frameId" id="frameId">
-            <input type="file" name="excel" required>
-            <input type="submit" id="ajaxSubmit">
-        </form>
-    </div>
-</div>
 <script>
     $(function () {
         //给id为ajaxSubmit的按钮提交表单
-        $("#ajaxSubmit").on("click",function () {
+        $("#ajaxSubmit").on("click", function () {
             //alert(1);
             $("#ajaxForm").ajaxSubmit({
-                beforeSubmit:function () {
+                beforeSubmit: function () {
                     // alert("我在提交表单之前被调用！");
                 },
-                success:function (data) {
+                success: function (data) {
                     //alert("我在提交表单成功之后被调用");
                     handle(data);
                 }
@@ -31,12 +36,13 @@
             return false;
         });
     });
+
     //处理返回数据
-    function handle(data){
-        if(data.status == 200){
+    function handle(data) {
+        if (data.status == 200) {
             alert(data.message);
             //处理逻辑
-        }else{
+        } else {
             alert(data.message);
             //处理逻辑
         }
